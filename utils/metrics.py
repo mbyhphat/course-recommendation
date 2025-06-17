cb70b440b6bf9bd117ecce529a3f0a36d714844a
mbyhphat <hhp031104ft@gmail.com>
2025-06-17 20:44:21 +0700

    feat: negative sampling

--- utils/metrics.py
+++ utils/metrics.py
@@ -36,7 +36,7 @@ def average_precision(hit, cut):
     hit = np.asarray(hit)
     precisions = [precision_at_k(hit, k + 1) for k in range(cut) if len(hit) >= k]
     if not precisions:
-        return 0.
+        return 0.0
     return np.sum(precisions) / float(min(cut, np.sum(hit)))
 
 
@@ -46,7 +46,7 @@ def dcg_at_k(rel, k):
     rel: list, element is positive real values, can be binary
     """
     rel = np.asfarray(rel)[:k]
-    dcg = np.sum((2 ** rel - 1) / np.log2(np.arange(2, rel.size + 2)))
+    dcg = np.sum((2**rel - 1) / np.log2(np.arange(2, rel.size + 2)))
     return dcg
 
 
@@ -57,7 +57,7 @@ def ndcg_at_k(rel, k):
     """
     idcg = dcg_at_k(sorted(rel, reverse=True), k)
     if not idcg:
-        return 0.
+        return 0.0
     return dcg_at_k(rel, k) / idcg
 
 
@@ -67,13 +67,13 @@ def ndcg_at_k_batch(hits, k):
     hits: array, element is binary (0 / 1), 2-dim
     """
     hits_k = hits[:, :k]
-    dcg = np.sum((2 ** hits_k - 1) / np.log2(np.arange(2, k + 2)), axis=1)
+    dcg = np.sum((2**hits_k - 1) / np.log2(np.arange(2, k + 2)), axis=1)
 
     sorted_hits_k = np.flip(np.sort(hits), axis=1)[:, :k]
-    idcg = np.sum((2 ** sorted_hits_k - 1) / np.log2(np.arange(2, k + 2)), axis=1)
+    idcg = np.sum((2**sorted_hits_k - 1) / np.log2(np.arange(2, k + 2)), axis=1)
 
     idcg[idcg == 0] = np.inf
-    ndcg = (dcg / idcg)
+    ndcg = dcg / idcg
     return ndcg
 
 
@@ -91,7 +91,7 @@ def recall_at_k_batch(hits, k):
     calculate Recall@k
     hits: array, element is binary (0 / 1), 2-dim
     """
-    res = (hits[:, :k].sum(axis=1) / hits.sum(axis=1))
+    res = hits[:, :k].sum(axis=1) / hits.sum(axis=1)
     return res
 
 
@@ -99,14 +99,14 @@ def F1(pre, rec):
     if pre + rec > 0:
         return (2.0 * pre * rec) / (pre + rec)
     else:
-        return 0.
+        return 0.0
 
 
 def calc_auc(ground_truth, prediction):
     try:
         res = roc_auc_score(y_true=ground_truth, y_score=prediction)
     except Exception:
-        res = 0.
+        res = 0.0
     return res
 
 
@@ -115,45 +115,58 @@ def logloss(ground_truth, prediction):
     return logloss
 
 
-def calc_metrics_at_k(cf_scores, train_user_dict, test_user_dict, user_ids, item_ids, Ks, num_negatives=100):
-    '''
+def calc_metrics_at_k_with_negative_sampling(
+    cf_scores,
+    train_user_dict,
+    test_user_dict,
+    user_ids,
+    item_ids,
+    Ks,
+    num_negatives=100,
+):
+    """
     Calculate precision, recall, and NDCG at K for each user with negative sampling.
     Negative sampling selects 100 items that are neither the ground truth item nor in the user's train set.
 
     cf_scores: (n_users, n_items)
-    '''
+    """
 
     binary_hit = []
     temp_cf_scores = []
     test_indices = []
 
-    test_pos_item_binary = np.concatenate((
-        np.ones((len(user_ids), 1)),
-        np.zeros((len(user_ids), num_negatives))
-    ), axis=1)
+    test_pos_item_binary = np.concatenate(
+        (np.ones((len(user_ids), 1)), np.zeros((len(user_ids), num_negatives))), axis=1
+    )
 
-
-    print('Hello')
     for idx, user in enumerate(user_ids):
         # Ground truth items for the user
         test_item = set(test_user_dict[user])
-        
+
         # Items in the training set to be excluded
         train_items = set(train_user_dict[user])
-        
+
         # Negative samples: items not in the test items and not in the train items
-        possible_negatives = [item for item in item_ids if item not in train_items and item not in test_item]
-        negative_samples = np.random.choice(possible_negatives, num_negatives, replace=False)
-        
+        possible_negatives = [
+            item
+            for item in item_ids
+            if item not in train_items and item not in test_item
+        ]
+        negative_samples = np.random.choice(
+            possible_negatives, num_negatives, replace=False
+        )
+
         # Selected items for testing: ground truth + negative samples
         test_set = list(test_item) + list(negative_samples)
         # test_indices.append(test_set)
 
         # Get the corresponding scores of these items from the cf_scores matrix
         temp_cf_scores.append(cf_scores[idx][test_set].tolist())
-        
+
     try:
-        _, rank_indices = torch.sort(torch.LongTensor(temp_cf_scores).cuda(), descending=True)    # try to speed up the sorting process
+        _, rank_indices = torch.sort(
+            torch.LongTensor(temp_cf_scores).cuda(), descending=True
+        )  # try to speed up the sorting process
     except:
         _, rank_indices = torch.sort(torch.LongTensor(temp_cf_scores), descending=True)
 
@@ -161,7 +174,7 @@ def calc_metrics_at_k(cf_scores, train_user_dict, test_user_dict, user_ids, item
 
     # binary_hit = [] # shape (n_users, num_negatives+1)
     # test_indices = np.asarray(test_indices)
-    
+
     for i in range(len(user_ids)):
         binary_hit.append(test_pos_item_binary[i][rank_indices[i]])
     binary_hit = np.array(binary_hit, dtype=np.float32)
@@ -169,8 +182,43 @@ def calc_metrics_at_k(cf_scores, train_user_dict, test_user_dict, user_ids, item
     metrics_dict = {}
     for k in Ks:
         metrics_dict[k] = {}
-        metrics_dict[k]['precision'] = precision_at_k_batch(binary_hit, k)
-        metrics_dict[k]['recall']    = recall_at_k_batch(binary_hit, k)
-        metrics_dict[k]['ndcg']      = ndcg_at_k_batch(binary_hit, k)
+        metrics_dict[k]["precision"] = precision_at_k_batch(binary_hit, k)
+        metrics_dict[k]["recall"] = recall_at_k_batch(binary_hit, k)
+        metrics_dict[k]["ndcg"] = ndcg_at_k_batch(binary_hit, k)
 
+    return metrics_dict
+
+
+def calc_metrics_at_k(
+    cf_scores, train_user_dict, test_user_dict, user_ids, item_ids, Ks
+):
+    """
+    cf_scores: (n_users, n_items)
+    """
+    test_pos_item_binary = np.zeros([len(user_ids), len(item_ids)], dtype=np.float32)
+    for idx, u in enumerate(user_ids):
+        train_pos_item_list = train_user_dict[u]
+        test_pos_item_list = test_user_dict[u]
+        cf_scores[idx][train_pos_item_list] = -np.inf
+        test_pos_item_binary[idx][test_pos_item_list] = 1
+
+    try:
+        _, rank_indices = torch.sort(
+            cf_scores.cuda(), descending=True
+        )  # try to speed up the sorting process
+    except:
+        _, rank_indices = torch.sort(cf_scores, descending=True)
+    rank_indices = rank_indices.cpu()
+
+    binary_hit = []
+    for i in range(len(user_ids)):
+        binary_hit.append(test_pos_item_binary[i][rank_indices[i]])
+    binary_hit = np.array(binary_hit, dtype=np.float32)
+
+    metrics_dict = {}
+    for k in Ks:
+        metrics_dict[k] = {}
+        metrics_dict[k]["precision"] = precision_at_k_batch(binary_hit, k)
+        metrics_dict[k]["recall"] = recall_at_k_batch(binary_hit, k)
+        metrics_dict[k]["ndcg"] = ndcg_at_k_batch(binary_hit, k)
     return metrics_dict
